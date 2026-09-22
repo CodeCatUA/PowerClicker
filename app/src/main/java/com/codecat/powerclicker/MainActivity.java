22732a78e66937ecb5c5443c6cdee143224a3c00
Amid <codecat.ua@gmail.com>
2026-09-22 20:23:07 +0300

    added textView

--- app/src/main/java/com/codecat/powerclicker/MainActivity.java
+++ app/src/main/java/com/codecat/powerclicker/MainActivity.java
@@ -1,6 +1,8 @@
 package com.codecat.powerclicker;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +11,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int player1Score = 0;
+    private int player2Score = 0;
+
+    private TextView tvScore1, tvScore2;
+    private ImageView imageView1, imageView2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +28,20 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        tvScore1 = findViewById(R.id.tvScore1);
+        tvScore2 = findViewById(R.id.tvScore2);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+
+        imageView1.setOnClickListener(v ->{
+            player1Score++;
+            tvScore1.setText("" + player1Score);
+        });
+        imageView2.setOnClickListener(v ->{
+            player2Score++;
+            tvScore2.setText("" + player2Score);
         });
     }
 }
